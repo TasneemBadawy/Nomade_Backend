3c73966f0c5b1cde185fb0608d2d927ad2331bef
Nermeen Safwat <nermeensafwat27@gmail.com>
2026-05-24 10:17:37 +0300

    touristModel.js

--- models/touristModel.js
+++ models/touristModel.js
@@ -1,48 +1,41 @@
-import db from '../config/database';
+import db from "../config/database.js";
 
 // find by email function
 
-const findTouristByEmail = (Email) =>{
-     return new Promise((resolve , reject) =>{
+const findTouristByEmail = (Email) => {
+  return new Promise((resolve, reject) => {
+    // the query that will search
+    const sql = `SELECT * FROM User
+        WHERE Email = ? `;
 
-        // the qyery that will search 
-        const sql =  `SELECT * FROM Users
-        WHERE Email = ?
-        `;
-
-        db.qyery(sql , [Email] , (err , result) =>{
-
-            if(err){
-                reject(err);
-            }else{
-                resolve(result[0]);
-            }
-        });
-     });
+    db.query(sql, [Email], (err, result) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(result[0]);
+      }
+    });
+  });
 };
-
 
 // create a user
 
-const createTourist = (FName , LName , Email , Password) =>{
+const createTourist = (FName, LName, Email, Password) => {
+  return new Promise((resolve, reject) => {
+    // the query we will need
 
-    return new Promise((resolve , reject) =>{
-
-        // the qyery we will need
-
-        const sql = `
-        INSERT INTO Users
+    const sql = `
+        INSERT INTO User
         (FName, LName, Email, Password)
         VALUES (?, ?, ?, ?)
         `;
 
-        db.qyery(sql , [FName , LName , Email , Password] , (err , result) =>{
-               
-             if(err){
-                reject(err);
-            }else{
-                resolve(result[0]);;
-            }
-        });
+    db.query(sql, [FName, LName, Email, Password], (err, result) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(result[0]);
+      }
     });
+  });
 };
