e7995779959add1d1b1ef62c7f48baf15a76bbcf
Nermeen Safwat <nermeensafwat27@gmail.com>
2026-05-24 10:23:03 +0300

    server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-import express from 'express';
-import { config } from 'dotenv';
-
+import express from "express";
+import { config } from "dotenv";
+import authRoutes from "./routes/authRoutes.js";
 const app = express();
 
 app.use(express.json());
