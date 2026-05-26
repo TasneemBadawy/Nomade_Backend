ff54cb61cb268f5ea07dd5a6f6c425053cb08878
Nermeen Safwat <nermeensafwat27@gmail.com>
2026-05-26 12:19:23 +0300

    generateToken.js

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -2,7 +2,10 @@ import jwt from "jsonwebtoken";
 
 // the function that generates the token
 const generateToken = (email) => {
-  return jwt.sign({ id: email }, process.env.jwt, { expiresIn: '1d' });
+  return jwt.sign(
+    { id: email },
+  process.env.jwt, 
+{expiresIn: '1d' });
 };
 
 export default generateToken;
