e2d904da8f880ad8a4f31f99b7ddfc06f36e3dae
Nermeen Safwat <nermeensafwat27@gmail.com>
2026-05-28 14:19:14 +0300

    server.js

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 import express from "express";
 import { config } from "dotenv";
 import authRoutes from "./routes/authRoutes.js";
-const app = express();
+import tourRoutes from "./routes/tourRoutes.js";
 
+config();
+
+const app = express();
 app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+app.use("/api", tourRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
