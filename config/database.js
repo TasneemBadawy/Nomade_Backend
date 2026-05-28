dc14d1e17dcb9843e11432bc1a0b2f1a7c06410a
Nermeen Safwat <nermeensafwat27@gmail.com>
2026-05-28 14:14:06 +0300

    database.js

--- config/database.js
+++ config/database.js
@@ -1,6 +1,7 @@
-const express = require("express");
-const { createPool } = require("mysql2");
-import dotenv from 'dotenv';
+//const express = require("express");
+import express from "express";
+import { createPool } from "mysql2";
+import dotenv from "dotenv";
 dotenv.config();
 const app = express();
 const port = 3000;
@@ -10,7 +11,7 @@ const pool = createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   port: 3307,
-  database:process.env.DB_NAME,
+  database: process.env.DB_NAME,
   connectionLimit: 10,
 });
 
@@ -27,3 +28,5 @@ app.get("/users", (req, res) => {
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
+
+export default app;
