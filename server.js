beaf4a4f6156a26cf8ad7b34b1068d1e9d17a9cd
Nermeen Safwat <nermeensafwat27@gmail.com>
2026-06-25 17:40:36 +0300

    server.js

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ import authRoutes from "./routes/authRoutes.js";
 import tourRoutes from "./routes/tourRoutes.js";
 import reviewRoutes from './routes/reviewRoutes.js';
 import bookingRoutes from './routes/bookingRoutes.js';
+import activityRoutes from "./routes/activityRoutes.js";
+import guideRoutes from "./routes/guideRoutes.js";
 config();
 
 const app = express();
@@ -13,6 +15,8 @@ app.use("/api/auth", authRoutes);
 app.use("/api", tourRoutes);
 app.use('/reviews' , reviewRoutes);
 app.use('/booking' , bookingRoutes);
+app.use("/api", activityRoutes);
+app.use("/api", guideRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
