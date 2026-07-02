34316af65cd7503fe74ef107b754e5db7d83cb7c
Nermeen Safwat <nermeensafwat27@gmail.com>
2026-07-02 16:03:33 +0300

    tourModel.js

--- models/tourModel.js
+++ models/tourModel.js
@@ -7,12 +7,12 @@ export const createTour = (
   Country,
   City,
   Street,
- Description,
+  Description,
   Days,
   Nights,
 ) => {
   return new Promise((resolve, reject) => {
-    const sql = `INSERT INTO Tours(Tour_name, Price_per_person,Country , City, Street, Description,Days,Nights)VALUES (?, ?, ?, ?, ?, ?, ?,?)`;
+    const sql = `INSERT INTO Tours(Tour_name, Price_per_person,Country , City, Street, tour_Description,Days,Nights)VALUES (?, ?, ?, ?, ?, ?, ?,?)`;
 
     db.query(
       sql,
@@ -70,6 +70,35 @@ export const deleteTourById = (Tour_ID) => {
     db.query(sql, [Tour_ID], (err, result) => {
       if (err) return reject(err);
       resolve(result);
+    });
+  });
+};
+
+/*************************Search and Filter Tours*********************/
+
+export const searchAndFilterTours = (Country, City, Price_per_person) => {
+  return new Promise((resolve, reject) => {
+    let sql = `SELECT * FROM Tours WHERE 1=1`;
+    const params = [];
+
+    if (Country) {
+      sql += ` AND Country LIKE ?`;
+      params.push(`%${Country}%`);
+    }
+
+    if (City) {
+      sql += ` AND City LIKE ?`;
+      params.push(`%${City}%`);
+    }
+
+    if (Price_per_person) {
+      sql += ` AND Price_per_person <= ?`;
+      params.push(Number(Price_per_person));
+    }
+
+    db.query(sql, params, (err, results) => {
+      if (err) return reject(err);
+      resolve(results);
     });
   });
 };
