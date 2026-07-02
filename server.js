a1f724aa60c6362ad6867240a09b46297f4db3b2
Nermeen Safwat <nermeensafwat27@gmail.com>
2026-07-02 16:08:37 +0300

    server.js

--- server.js
+++ server.js
@@ -25,10 +25,10 @@ const swaggerSpec = swaggerJsdoc(options);*/
 
 app.use("/api/auth", authRoutes);
 app.use("/api", tourRoutes);
-app.use('/reviews' , reviewRoutes);
-app.use('/booking' , bookingRoutes);
 app.use("/api", activityRoutes);
 app.use("/api", guideRoutes);
+app.use("/reviews", reviewRoutes);
+app.use("/booking", bookingRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
